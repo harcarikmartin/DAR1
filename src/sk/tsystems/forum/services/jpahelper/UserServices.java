9056e0697c6d63b4523c72c9f162e4b74ab0d37b
slavobarila <slavo.barilla01@gmail.com>
2016-08-03 09:31:02 +0200

    Pridanie useServices

---
+++ src/sk/tsystems/forum/services/jpahelper/UserServices.java
@@ -0,0 +1,23 @@
+package sk.tsystems.forum.services.jpahelper;
+
+import java.sql.Date;
+import javax.persistence.EntityManager;
+//import sk.tsystems.forum.entities.User;
+
+public class UserServices {
+
+	
+	
+	
+	public void addUser(String userName, String userPassword, Date birthDate) {
+
+		EntityManager em = JpaHelper.getEntityManager();
+		
+		JpaHelper.beginTransaction();
+//		em.persist(new User());
+		JpaHelper.commitTransaction();
+
+	}
+	
+	
+}
