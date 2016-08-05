c66297ce0343ba53e550cba8a700f6db687e96d6
KarolKlescinsky <klescinsky.karol@gmail.com>
2016-08-05 13:06:07 +0200

    Adding TopicServices.

---
+++ src/sk/tsystems/forum/services/TopicServices.java
@@ -0,0 +1,31 @@
+package sk.tsystems.forum.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import sk.tsystems.forum.entities.Topic;
+import sk.tsystems.forum.services.jpahelper.JpaHelper;
+
+public class TopicServices {
+
+	public List<Topic> printTopic(Topic topic) {
+
+		List<Topic> listOfTopics = new ArrayList<>();
+		EntityManager em = JpaHelper.getEntityManager();
+		Query query = em.createQuery("select t from Topic t");
+		listOfTopics = query.getResultList();
+		return listOfTopics;
+
+	}
+	
+	public void addTopicToDatabase(Topic topic) {
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		em.persist(topic);
+		JpaHelper.commitTransaction();
+	}
+
+}
