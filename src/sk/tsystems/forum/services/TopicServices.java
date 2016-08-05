6248539579c4cd868bf5a4f910619e70a76d786f
KarolKlescinsky <klescinsky.karol@gmail.com>
2016-08-05 14:16:37 +0200

    Adding new method to TopicServices.

--- src/sk/tsystems/forum/services/TopicServices.java
+++ src/sk/tsystems/forum/services/TopicServices.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import sk.tsystems.forum.entities.Topic;
+import sk.tsystems.forum.entities.User;
 import sk.tsystems.forum.services.jpahelper.JpaHelper;
 
 public class TopicServices {
@@ -16,6 +17,17 @@ public class TopicServices {
 		List<Topic> listOfTopics = new ArrayList<>();
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select t from Topic t");
+		listOfTopics = query.getResultList();
+		return listOfTopics;
+
+	}
+	
+	public List<Topic> printSpecificTopicsByUsersID(User user) {
+
+		List<Topic> listOfTopics = new ArrayList<>();
+		EntityManager em = JpaHelper.getEntityManager();
+		Query query = em.createNativeQuery("select t.topic from TOPIC_USER tu join TOPIC t on t.TOPICID=tu.TOPIC_ID join USERS u on tu.USER_ID=u.USERID where u.USERID LIKE:userID;");
+		query.setParameter("userID", user.getUserID());
 		listOfTopics = query.getResultList();
 		return listOfTopics;
 
