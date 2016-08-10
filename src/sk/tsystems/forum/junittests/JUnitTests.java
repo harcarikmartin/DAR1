0a01e7090892cfb04500b87a442920d1d622465e
KarolKlescinsky <klescinsky.karol@gmail.com>
2016-08-10 11:11:00 +0200

    Multiple improvements of several JUnit tests and adding new JUnit tests.

--- src/sk/tsystems/forum/junittests/JUnitTests.java
+++ src/sk/tsystems/forum/junittests/JUnitTests.java
@@ -18,21 +18,21 @@ public class JUnitTests {
 
 	TopicServices topicServices = new TopicServices();
 	UserServices userServices = new UserServices();
-	String tester = "tester";
-	String testingTopic = "testing topic";
+	String nameOfTester = "tester";
+	String nameOfTestingTopic = "testing topic";
 	User testedUser = new User();
 	Topic testedTopic = new Topic();
 
 	@Before
 	public void createTesterUserAndTestingTopic() {
-		testedUser.setUserName(tester);
+		testedUser.setUserName(nameOfTester);
 		testedUser.setUserPassword("tester");
 		testedUser.setBirthDate(null);
 		testedUser.setRole("user");
 		testedUser.setStatus("pending");
 		userServices.addUser(testedUser);
 
-		testedTopic.setTopic(testingTopic);
+		testedTopic.setTopic(nameOfTestingTopic);
 		testedTopic.setVisibility("private");
 		testedTopic.setCreator(testedUser);
 		topicServices.addTopicToDatabase(testedTopic);
@@ -40,39 +40,62 @@ public class JUnitTests {
 
 	@After
 	public void dropUser() {
-		topicServices.removeTopic(testingTopic);
-		userServices.dropUser(tester);
+		topicServices.removeTopic(nameOfTestingTopic);
+		userServices.dropUser(nameOfTester);
 	}
 
 	@Test
+	public void doesMethodAddUserWork() {
+		// Checks if user "tester" is in database
+		assertEquals("tester", userServices.setPresentUser(nameOfTester).getUserName());
+	}
+
+	@Test
+	public void doesMethodApproveUserWork(){
+		String statusBeforeUpdate = userServices.setPresentUser(nameOfTester).getStatus();
+		userServices.approveUser(nameOfTester);
+		String statusAfterUpdate = userServices.setPresentUser(nameOfTester).getStatus();
+		assertNotEquals(statusBeforeUpdate, statusAfterUpdate);
+	}
+	
+	@Test
 	public void doesMethodIsPasswordCorrectWork() {
 		// Good password
-		assertTrue(userServices.isPasswordCorrect(tester, "tester"));
+		assertTrue(userServices.isPasswordCorrect(nameOfTester, "tester"));
 		// bad password
-		assertFalse(userServices.isPasswordCorrect(tester, "testerios"));
+		assertFalse(userServices.isPasswordCorrect(nameOfTester, "testerios"));
 	}
 
 	@Test
 	public void deosMethodIsUserApprovedWork() {
 		// Checks if is user "tester" approved
-		assertFalse(userServices.isUserApproved(tester));
+		assertFalse(userServices.isUserApproved(nameOfTester));
 	}
 
 	@Test
 	public void doesmethodGetUserIDWork() {
 		// Checks if user "tester" was created
-		assertNotEquals(0, userServices.getUserID(tester));
+		assertNotEquals(0, userServices.getUserID(nameOfTester));
 	}
 
 	@Test
 	public void doesMethodSetPresentUserWork() {
 		// Checks if user "tester" is in database
-		assertNotEquals(null, userServices.setPresentUser(tester));
+		assertEquals("tester", userServices.setPresentUser(nameOfTester).getUserName());
+	}
+
+	@Test
+	public void doesMethodAddTopicToDatabase() {
+		// Checks if topic "testing topic" is in database
+		assertEquals("testing topic", topicServices.setPresentTopic("testing topic").getTopic());
 	}
 
 	@Test
 	public void doesMethodPrintTopicsWork() {
-		// Firstly, prints topics from database to listOfTestedTopics. Secondly, adds one more topic to database and prints to listOfTestedTopics1. Finally, compares lists. 
+		// Firstly, prints topics from database to listOfTestedTopics.
+		// Secondly, adds one more topic to database and prints to
+		// listOfTestedTopics1.
+		// Finally, compares lists.
 		List<Topic> listOfTestedTopics = new ArrayList<>();
 		listOfTestedTopics = topicServices.printTopics();
 		topicServices.addTopicToDatabase(new Topic(null, null, null, "Bonus"));
@@ -84,32 +107,23 @@ public class JUnitTests {
 
 	@Test
 	public void doesMethodGetTopicIDWork() {
-		// Checks if topic "testingTopic" was created
-		assertNotEquals(0, topicServices.getTopicID(testingTopic));
+		// Checks if topic "testing Topic" was created
+		assertNotEquals(0, topicServices.getTopicID(nameOfTestingTopic));
 	}
 
 	@Test
 	public void doesMethodSetPresentTopicWork() {
-		// Checks if topic "testingTopic" is in database
-		assertNotEquals(null, topicServices.setPresentTopic(testingTopic));
+		// Checks if topic "testing Topic" is in database
+		assertEquals(nameOfTestingTopic, topicServices.setPresentTopic(nameOfTestingTopic).getTopic());
 	}
 
-	// TODO: Bazmeg nefunguje to more
 	@Test
 	public void doesUpdateTopicMethodWorks() {
 		// Checks if method updateTopic works
-		System.out.println(testedTopic);
-		Topic testedTopic1 = new Topic();
-		testedTopic1 = new TopicServices().setPresentTopic(testingTopic);
-		Topic testedTopic2 = new Topic();
-		new TopicServices().updateTopic(testingTopic, testingTopic, null);
-		System.out.println(testedTopic);
-		testedTopic2 = new TopicServices().setPresentTopic(testingTopic);
-		System.out.println(testedTopic1);
-		System.out.println(testedTopic2);
-		System.out.println(testedTopic);
-		assertEquals(testedTopic1, testedTopic2);
-
+		String vissibilityBeforeUpdate = new TopicServices().setPresentTopic(nameOfTestingTopic).getVisibility();
+		new TopicServices().updateTopic(nameOfTestingTopic, nameOfTestingTopic, null);
+		String vissibilityAfterUpdate = new TopicServices().setPresentTopic(nameOfTestingTopic).getVisibility();
+		assertNotEquals(vissibilityBeforeUpdate, vissibilityAfterUpdate);
 	}
 
 }
