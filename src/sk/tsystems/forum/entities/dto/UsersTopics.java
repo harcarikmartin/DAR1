b7c243972115e11b52e96f33151a91bc14a3b10f
KarolKlescinsky <klescinsky.karol@gmail.com>
2016-08-11 09:17:08 +0200

    removing another blank spaces.

--- src/sk/tsystems/forum/entities/dto/UsersTopics.java
+++ src/sk/tsystems/forum/entities/dto/UsersTopics.java
@@ -4,41 +4,39 @@ import sk.tsystems.forum.entities.Topic;
 import sk.tsystems.forum.entities.User;
 
 public class UsersTopics {
-	
+
 	private User user;
 	private Topic topic;
-	
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public Topic getTopic() {
 		return topic;
 	}
+
 	public void setTopic(Topic topic) {
 		this.topic = topic;
 	}
-	
+
 	public UsersTopics(User user, Topic topic) {
 		this.user = user;
 		this.topic = topic;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "UsersTopics [topicID=" + topic.getTopicID() + ", "
-							+ "topicName=" + topic.getTopic() + ", "
-							+ "topicVisibility=" + topic.getVisibility() + ", "
-							+ "topicCreatorName=" + topic.getCreator().getUserName() + ", "
-							+ "topicCreatorID=" + topic.getCreator().getUserID() + ", "
-							+ "userID=" + user.getUserName() + ", "
-							+ "userName=" + user.getUserName() + ", "
-							+ "userPassword=" + user.getUserPassword() + ", "
-							+ "userRole=" + user.getRole() + ", "
-							+ "userStatus=" + user.getStatus() + ", "
-							+ "userBirthdate=" + user.getBirthDate() + "]";
+		return "UsersTopics [topicID=" + topic.getTopicID() + ", " + "topicName=" + topic.getTopic() + ", "
+				+ "topicVisibility=" + topic.getVisibility() + ", " + "topicCreatorName="
+				+ topic.getCreator().getUserName() + ", " + "topicCreatorID=" + topic.getCreator().getUserID() + ", "
+				+ "userID=" + user.getUserName() + ", " + "userName=" + user.getUserName() + ", " + "userPassword="
+				+ user.getUserPassword() + ", " + "userRole=" + user.getRole() + ", " + "userStatus=" + user.getStatus()
+				+ ", " + "userBirthdate=" + user.getBirthDate() + "]";
 	}
 
 }
