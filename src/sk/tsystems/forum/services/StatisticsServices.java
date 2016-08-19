37da5bcda6eeb9103ee838723f667135b677330a
harcarikmartin <martinharcarik@147.232.168.253>
2016-08-19 15:15:34 +0200

    doc comments added for the methods of the class StatisticsServices

--- src/sk/tsystems/forum/services/StatisticsServices.java
+++ src/sk/tsystems/forum/services/StatisticsServices.java
@@ -1,17 +1,22 @@
 package sk.tsystems.forum.services;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
+import sk.tsystems.forum.entities.Comment;
+import sk.tsystems.forum.entities.Task;
+import sk.tsystems.forum.entities.Topic;
 import sk.tsystems.forum.entities.User;
 import sk.tsystems.forum.services.jpahelper.JpaHelper;
 
 public class StatisticsServices {
 	
+	/**
+	 * Gets the amount of instances of {@link User} class stored in database.
+	 * 
+	 * @return count of users
+	 */
 	public int getAllUsersCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from User u");
@@ -23,6 +28,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the amount of instances of {@link User} class stored in database, 
+	 * that have the property role set to 'admin'.
+	 * 
+	 * @return count of administrators
+	 */
 	public int getAdminsCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from User u where u.role = 'admin'");
@@ -34,6 +45,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the amount of instances of {@link User} class stored in database, 
+	 * that have the property role set to 'user'.
+	 * 
+	 * @return count of users
+	 */
 	public int getUsersCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from User u where u.role = 'user'");
@@ -45,6 +62,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the amount of instances of {@link User} class stored in database, 
+	 * that have the property status set to 'confirmed'.
+	 * 
+	 * @return count of approved users
+	 */
 	public int getApprovedCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from User u where u.status = 'confirmed'");
@@ -56,6 +79,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the amount of instances of {@link User} class stored in database, 
+	 * that have the property status set to 'pending'.
+	 * 
+	 * @return count of not approved users
+	 */
 	public int getNotApprovedCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from User u where u.status = 'pending'");
@@ -67,6 +96,11 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Get the amount of instances of {@link Topic} class stored in database.
+	 * 
+	 * @return count of topics
+	 */
 	public int getTopicsCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from Topic");
@@ -78,6 +112,11 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Get the amount of instances of {@link Task} class stored in database.
+	 * 
+	 * @return count of tasks
+	 */
 	public int getTasksCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from Task");
@@ -89,6 +128,11 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Get the amount of instances of {@link Comment} class stored in database.
+	 * 
+	 * @return count of comments
+	 */
 	public int getCommentsCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from Comment");
@@ -100,6 +144,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Get the amount of instances of {@link Topic} class stored in database, 
+	 * that have the property visibility set to 'public'.
+	 * 
+	 * @return count of public topics
+	 */
 	public int getPublicTopicsCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from Topic t where t.visibility = 'public'");
@@ -111,6 +161,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Get the amount of instances of {@link Topic} class stored in database, 
+	 * that have the property visibility set to 'private'.
+	 * 
+	 * @return count of private topics
+	 */
 	public int getPrivateTopicsCount() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(*) from Topic t where t.visibility = 'private'");
@@ -122,6 +178,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the name property of the instance of {@link Task} class, 
+	 * that have the highest number of instances of {@link Comment} class assigned to or null.
+	 * 
+	 * @return name of the task, that have the highest amount of comments assigned to
+	 */
 	public String getMostAnsweredTask() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select c.task.taskName from Comment c group by c.task.taskName order by count(c.task.taskName) desc");
@@ -133,6 +195,11 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the highest number of instances of {@link Comment} class that are assigned to the instance of {@link Task} class.
+	 * 
+	 * @return number of comments for task that has the highest amount of comments assigned to
+	 */
 	public int getNumberOfCommentsForMostAnsweredTask() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select max(count(c.task.taskName)) from Comment c group by c.task.taskName order by count(c.task.taskName) desc");
@@ -151,6 +218,12 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the userName property of the instance of {@link User} class which has 
+	 * the highest number of instances of {@link Comment} class assigned to.
+	 * 
+	 * @return name of user, which has the highest number of comments assigned to
+	 */
 	public String getMostActiveUser() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select c.user.userName from Comment c group by c.user.userName order by count(c.user.userName) desc");
@@ -162,6 +235,11 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the highest number of instances of {@link Comment} class assigned to the instance of {@link User} class.
+	 * 
+	 * @return number of comments for user that has the highest amount of comments assigned to
+	 */
 	public int getNumberOfCommentsForMostActiveUser() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select max(count(c.user.userName)) from Comment c group by c.user.userName order by count(c.user.userName) desc");
@@ -180,6 +258,13 @@ public class StatisticsServices {
 		}
 	}
 	
+	/**
+	 * Gets the number of instances of {@link Task} class for specified instance of {@link Topic}. Topic is specified 
+	 * by parameter topic of type String, that represents property topic of class Topic.
+	 * 
+	 * @param topic	the name of topic to get amount of tasks for
+	 * @return number of tasks assigned to specific topic
+	 */
 	public int getNumberOfTasksForTopic(String topic) {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select count(t) from Task t where t.topic.topic = :topic");
@@ -191,7 +276,13 @@ public class StatisticsServices {
 			return Math.toIntExact((long) query.getResultList().get(0));
 		}
 	}
-
+	
+	/**
+	 * Gets the instance of {@link User}, that has the property registeredOn with highest value, 
+	 * or null if there are no instances of User.
+	 * 
+	 * @return User, that has registered as last or null, if there are no users to select from
+	 */
 	public User getLatestUser() {
 		EntityManager em = JpaHelper.getEntityManager();
 		Query query = em.createQuery("select u from User u order by u.registeredOn desc");
@@ -201,9 +292,15 @@ public class StatisticsServices {
 		} else {
 			return (User) query.getResultList().get(0);
 		}
-		
 	}
-
+	
+	/**
+	 * Gets the {@link List} of instances of {@link User} class, that have the property registeredOn 
+	 * set not longer than 7 days ago from the moment of calling the method. Property's registeredOn type is 
+	 * {@link Date}.
+	 * 
+	 * @return list of users that have registered not longer than a week ago from the moment of calling the method
+	 */
 	public List<User> getRegisteredLastWeek() {
 		Date cal = new Date(System.currentTimeMillis() - 7*24*60*60*1000);
 		EntityManager em = JpaHelper.getEntityManager();
