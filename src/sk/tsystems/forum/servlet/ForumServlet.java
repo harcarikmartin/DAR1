b0ccc5088ba787fca9640781f987fda554ae655b
slavobarila <slavo.barilla01@gmail.com>
2016-08-22 08:18:51 +0200

    Diakritika forum servlet

--- src/sk/tsystems/forum/servlet/ForumServlet.java
+++ src/sk/tsystems/forum/servlet/ForumServlet.java
@@ -319,6 +319,8 @@ public class ForumServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 	}
 	
