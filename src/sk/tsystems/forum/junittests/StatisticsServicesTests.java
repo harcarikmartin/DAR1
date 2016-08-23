f88d49c8246eb0dfbe3efad6c4ec2e321494afb2
KarolKlescinsky <klescinsky.karol@gmail.com>
2016-08-23 12:47:54 +0200

    Adding documentation to StatisticsServicesTests class.

--- src/sk/tsystems/forum/junittests/StatisticsServicesTests.java
+++ src/sk/tsystems/forum/junittests/StatisticsServicesTests.java
@@ -12,6 +12,12 @@ import sk.tsystems.forum.services.TaskServices;
 import sk.tsystems.forum.services.TopicServices;
 import sk.tsystems.forum.services.UserServices;
 
+/**
+ * This class contains JUnit Tests which check if all methods in
+ * {@link StatisticsServices} work correctly.
+ * 
+ * @author karolklescinsky
+ */
 public class StatisticsServicesTests {
 
 	private TopicServices topicServices = new TopicServices();
@@ -21,6 +27,13 @@ public class StatisticsServicesTests {
 	private CreatingTestingSamples sample = new CreatingTestingSamples();
 	private StatisticsServices statisticsServices = new StatisticsServices();
 
+	/**
+	 * This method is called before each test. Calls method createTestedUser
+	 * which adds testedUser into the database. Calls method createTestedTopic
+	 * which adds testedTopic into the database. Calls method createTestedTask
+	 * which adds testedTask into the database. Calls method createTestedComment
+	 * which adds testedComment the into database.
+	 */
 	@Before
 	public void createTesterUserAndTestingTopic() {
 		sample.createTestedUser();
@@ -29,6 +42,13 @@ public class StatisticsServicesTests {
 		sample.createTestedComment();
 	}
 
+	/**
+	 * This method is called after each test. Calls method removeComment which
+	 * drops testedComment from database. Calls method removeTask which drops
+	 * testedTask from database. Calls method removeTopic which drops
+	 * testedTopic from database. Calls method dropUser which drops testedUser
+	 * from database.
+	 */
 	@After
 	public void dropUser() {
 		commentServices.removeComment(sample.getCommentID());
@@ -37,6 +57,11 @@ public class StatisticsServicesTests {
 		userServices.dropUser(sample.getNameOfTester());
 	}
 
+	/**
+	 * Checks if return of method getAllUsersCount is not null. Checks if return
+	 * of method getAllUsersCount is equal to sum of pending users and confirmed
+	 * users.
+	 */
 	@Test
 	public void doesMethodGetAllUsersCountWork() {
 		assertNotNull(statisticsServices.getAllUsersCount());
@@ -44,12 +69,21 @@ public class StatisticsServicesTests {
 				userServices.getPendingUsers().size() + userServices.getUsers().size());
 	}
 
+	/**
+	 * Checks if return of method getAdminsCount is equal to deduction of all
+	 * users count and users who are not administrators.
+	 */
 	@Test
 	public void doesMethodGetAdminsCountWork() {
 		assertEquals(statisticsServices.getAdminsCount(),
-				statisticsServices.getAllUsersCount() - statisticsServices.getAllUsersCount());
+				statisticsServices.getAllUsersCount() - statisticsServices.getUsersCount());
 	}
 
+	/**
+	 * Checks if return of method getUsersCount is not null. Checks if return of
+	 * method getUsersCount is equal to deduction of all users count and
+	 * administrators count.
+	 */
 	@Test
 	public void doesMethodGetUsersCountWork() {
 		assertNotNull(statisticsServices.getUsersCount());
@@ -57,6 +91,11 @@ public class StatisticsServicesTests {
 				statisticsServices.getAllUsersCount() - statisticsServices.getAdminsCount());
 	}
 
+	/**
+	 * Checks if return of method getApprovedCount is not null. Checks if return
+	 * of method getApprovedCount is equal to deduction of all users count and
+	 * not approved users count.
+	 */
 	@Test
 	public void doesMethodGetApprovedCountWork() {
 		assertNotNull(statisticsServices.getApprovedCount());
@@ -64,6 +103,11 @@ public class StatisticsServicesTests {
 				statisticsServices.getAllUsersCount() - statisticsServices.getNotApprovedCount());
 	}
 
+	/**
+	 * Checks if return of method getNotApprovedCount is not null. Checks if
+	 * return of method getNotApprovedCount is equal to deduction of all users
+	 * count and approved users count.
+	 */
 	@Test
 	public void doesMethodGetNotApprovedCountWork() {
 		assertNotNull(statisticsServices.getNotApprovedCount());
@@ -71,65 +115,105 @@ public class StatisticsServicesTests {
 				statisticsServices.getAllUsersCount() - statisticsServices.getApprovedCount());
 	}
 
+	/**
+	 * Checks if return of method getTopicsCount is not null. Checks if return
+	 * of method getTopicsCount is equal to size of list of all topics.
+	 */
 	@Test
 	public void doesMethodGetTopicsCountWork() {
 		assertNotNull(statisticsServices.getTopicsCount());
 		assertEquals(statisticsServices.getTopicsCount(), topicServices.printTopics().size());
 	}
 
+	/**
+	 * Checks if return of method getTasksCount is not null.
+	 */
 	@Test
 	public void doesMethodGetTasksCountWork() {
 		assertNotNull(statisticsServices.getTasksCount());
 	}
-
+	
+	/**
+	 * Checks if return of method getCommentsCount is not null.
+	 */
 	@Test
 	public void doesMethodGetCommentsCountWork() {
 		assertNotNull(statisticsServices.getCommentsCount());
 	}
 
+	/**
+	 *	Checks if return of method getPublicTopicsCount is equal to deduction of topics
+	 * count and private topics count.	
+	 */
 	@Test
 	public void doesMethodGetPublicTopicsCountWork() {
 		assertEquals(statisticsServices.getPublicTopicsCount(),
 				statisticsServices.getTopicsCount() - statisticsServices.getPrivateTopicsCount());
 	}
 
+	/**
+	 *	Checks if return of method getPrivateTopicsCount is equal to deduction of topics
+	 * count and public topics count.	
+	 */
 	@Test
 	public void doesMethodGetPrivateTopicsCountWork() {
 		assertEquals(statisticsServices.getPrivateTopicsCount(),
 				statisticsServices.getTopicsCount() - statisticsServices.getPublicTopicsCount());
 	}
 
+	/**
+	 * Checks if return of method getMostAnsweredTask is not null.
+	 */
 	@Test
 	public void doesMethodGetMostAnsweredTaskWork() {
 		assertNotNull(statisticsServices.getMostAnsweredTask());
 	}
-
+	
+	/**
+	 * Checks if return of method getNumberOfCommentsForMostAnsweredTask is not null.
+	 */
 	@Test
 	public void doesMethodGetNumberOfCommentsForMostAnsweredTaskWork() {
 		assertNotNull(statisticsServices.getNumberOfCommentsForMostAnsweredTask());
 	}
 
+	/**
+	 * Checks if return of method getMostActiveUser is not null.
+	 */
 	@Test
 	public void doesMethodGetMostActiveUserWork() {
 		assertNotNull(statisticsServices.getMostActiveUser());
 	}
 
+	/**
+	 * Checks if return of method getNumberOfCommentsForMostActiveUser is not null.
+	 */
 	@Test
 	public void doesMethodGetNumberOfCommentsForMostActiveUserWork() {
 		assertNotNull(statisticsServices.getNumberOfCommentsForMostActiveUser());
 	}
 
+	/**
+	 * Checks if return of method getNumberOfTasksForTopic is not null.
+	 * Checks if count of tasks in topic "tested topic" is equal to 1;
+	 */
 	@Test
 	public void doesMethodGetNumberOfTasksForTopicWork() {
 		assertNotNull(statisticsServices.getNumberOfTasksForTopic(sample.getNameOfTestingTopic()));
 		assertEquals(statisticsServices.getNumberOfTasksForTopic(sample.getNameOfTestingTopic()), 1);
 	}
 
+	/**
+	 * Checks if return of method getLatestUser is not null.
+	 */
 	@Test
 	public void doesMethodGetLatestUserWork() {
 		assertNotNull(statisticsServices.getLatestUser());
 	}
 
+	/**
+	 * Checks if return of method getRegisteredLastWeek is not null.
+	 */
 	@Test
 	public void doesMethodGetRegisteredLastWeekWork() {
 		assertNotNull(statisticsServices.getRegisteredLastWeek());
